5330f910aa4193362560b575b210065506fa62d8
Diego da Rosa <iamdiegoinacio@gmail.com>
2025-11-17 23:35:32 -0300

    feat(domain): define as interfaces de persistência (Repositórios e UoW)
    - Adiciona as abstrações da camada de persistência.
    - Define os contratos para IInvestimentoRepository, IProdutoInvestimentoRepository e ISimulacaoRepository.
    - Define a interface IUnitOfWork para garantir a atomicidade das transações.

---
+++ src/SIPRI.Domain/Interfaces/Persistence/ISimulacaoRepository.cs
@@ -0,0 +1,22 @@
+﻿using SIPRI.Domain.Entities;
+using SIPRI.Domain.Models;
+
+namespace SIPRI.Domain.Interfaces.Persistence;
+
+public interface ISimulacaoRepository
+{
+    /// <summary>
+    /// Adiciona uma nova simulação ao banco.
+    /// </summary>
+    Task AddAsync(Simulacao simulacao);
+
+    /// <summary>
+    /// Busca o histórico de todas as simulações.
+    /// </summary>
+    Task<IEnumerable<Simulacao>> GetAllAsync();
+
+    /// <summary>
+    /// Busca dados agregados das simulações.
+    /// </summary>
+    Task<IEnumerable<SimulacaoAgregada>> GetAgregadoPorDiaAsync();
+}
